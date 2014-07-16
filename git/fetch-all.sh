a4cd22b57489e2bbf7733fb17d6adc8ff66cd8cb
Jonathan Almeida <jonathan.almeida942@gmail.com>
2014-07-16 17:59:54 -0400

    Added fetch-all.sh
    - Given a directory of git repositories, this script fetches all the
      remotes and tries a rebase of the current branch. If it fails, it
      attempts a `stash`, `rebase` and `stash pop`.

---
+++ git/fetch-all.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Default git directory
+git_dir=~/git
+
+if [[ $1 != "" ]]; then
+    # Path is passed as first argument
+    ls $1 &>2
+    if [[ $? != 0 ]]; then
+        # Path isn't valid
+        echo "cannot access $1: No such directory"
+        exit
+    fi
+    git_dir=$1
+fi
+
+echo "Changing directory to $git_dir"
+cd $git_dir
+
+for dir in */; do
+    echo "=========================================="
+    echo $dir
+    cd $dir
+    echo "---"
+    echo "git fetch"
+    git fetch
+    echo "---"
+    echo "git rev-parse --abbrev-ref HEAD"
+    current_branch=`git rev-parse --abbrev-ref HEAD`
+    echo "current_branch = $current_branch"
+    echo "---"
+    echo "git rebase origin/$current_branch"
+    git rebase origin/$current_branch
+    if [[ $? != 0 ]]; then
+        echo "---"
+        echo "git stash"
+        git stash
+        echo "---"
+        echo "git rebase origin/$current_branch"
+        git rebase origin/$current_branch
+        second_attempt=$?
+        echo "---"
+        echo "git stash pop"
+        git stash pop
+    fi
+    if [[ second_attempt == 1 ]]; then
+        echo "Yo dawg, your repo ($dir.git) has problems.."
+    fi
+    echo "---"
+    echo "cd .."
+    cd ..
+done;
