1270ae4abf17c2d2a0687d5560308a62183d275e
Jonathan Almeida <jonathan.almeida942@gmail.com>
2014-07-19 18:20:41 -0400

    Organized options better using flags

--- git/fetch-all.sh
+++ git/fetch-all.sh
@@ -5,22 +5,59 @@ git_dir=~/git
 # Default remote
 git_remote="origin"
 
-if [[ $1 != "" ]]; then
-    # Path is passed as first argument
-    ls $1 &>2
-    if [[ $? != 0 ]]; then
-        # Path isn't valid
-        echo "cannot access $1: No such directory"
-        exit
-    fi
-    git_dir=$1
-fi
+function helpmenu {
+    echo "Usage example:"
+    echo "./fetch-all.sh ~/directory_of_git_repos origin"
+}
 
-if [[ $2 != "" ]]; then
-    # Second argument is not empty
-    # (Should contain a branch)
-    git_remote=$2
-fi
+function setDir {
+    if [[ $checkDir != "" ]]; then
+        # Path is passed as first argument
+        ls $checkDir &>2
+        if [[ $? != 0 ]]; then
+            # Path isn't valid
+            echo "cannot access $1: No such directory"
+            exit
+        fi
+        git_dir=$checkDir
+    fi
+}
+
+function setRemote {
+    if [[ $checkRemote != "" ]]; then
+        # Second argument is not empty
+        # (Should contain a branch)
+        git_remote=$checkRemote
+    fi
+}
+
+while getopts "hd:r:" OPTION
+do
+    case $OPTION in
+        h)
+            echo "You set flag -h"
+            helpmenu
+            exit
+            ;;
+        d)
+            echo "Sets directory of git repos"
+            checkDir=$OPTARG
+            setDir
+            break
+            ;;
+        r)
+            echo "Sets directory of git repos"
+            checkRemote=$OPTARG
+            setRemote
+            break
+            ;;
+        \?)
+            echo "Used for the help menu"
+            helpmenu
+            exit
+            ;;
+    esac
+done
 
 echo "Changing directory to $git_dir"
 cd $git_dir
